67c3a81fff256259d9bea5f954b9d9fadc5b03dc
Cleverson Dias <cleversonalvesdias@gmail.com>
2026-01-22 13:29:59 -0300

    feat: extração inicial de dados de clientes

---
+++ etl_clientes.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+clientes_dados = [
+    { "ID": 1,"Nome": "Claudia Machado","Status": "Ativo","Score": 740},
+    { "ID": 2,"Nome": "Jose Dias","Status": "Inativo","Score":579},
+    { "ID": 3,"Nome": "Mariana Silva","Status": "Ativo","Score": 925},
+    { "ID": 4,"Nome": "Paulo Souza","Status": "Ativo","Score": 610},
+]
+df_clientes = pd.DataFrame(clientes_dados)
+
+ 
+
+
+
+
+
